b23626d5a498f17bd2de3c6d2a8b2242ab901f7a
Nensan90 <85895812+Nensan90@users.noreply.github.com>
2021-11-01 14:06:02 -0400

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -5,3 +5,48 @@ var samples;
 var dataset;
 var metadata;
 
+d3.json(url).then(function(data) {
+    dataset = data;
+    metadata = data.metadata;
+    names = data.names;
+    samples = data.samples;
+
+    function init() {
+        var select = d3.select("select");
+        var initMetadata = metadata[0];
+        metadataPanelGen(initMetadata);
+        initSampleData = samples[0];
+        init();
+  });
+
+function metadataPanelGen(metadata) {
+    d3.selectAll("p").remove();
+    var mdPanel = d3.select('div.panel-body');
+    for (let [key, value] of Object.entries(metadata)) {
+      mdEntry = mdPanel.append("p").text(`${key}: ${value}`);
+      mdEntry.attr('valNum');
+    }
+  }
+
+  Plotly.newPlot('bar', barData);
+  var trace1 = {
+    x: sampleData.otu_ids,
+    y: sampleData.sample_values,
+    text: sampleData.otu_labels,
+    mode: 'markers',
+    marker: {
+      color: sampleData.otu_ids,  
+      size: sampleData.sample_values,
+    }
+  };
+
+  var bubbleData = [trace1];
+var bubbleLayout = { 
+    xaxis: {title: {text: 'OTU ID'}},
+    showlegend: false,
+    height: 400,
+    width: 800
+  };
+
+  Plotly.newPlot("bubblechart", bubbleData, bubbleLayout);
+}
